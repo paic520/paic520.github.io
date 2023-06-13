caa183efde5dbf8c47e75e56c0d1dc4dc8edb698
HZVVV <36513711+hzvvv@users.noreply.github.com>
2023-06-13 16:41:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ if (url == "/nets-tmr-pac-pad/viewCustomerVehicle.do") {
 //改	
 	new_element(url_No1 + "/editCustomerTel.js");
 	
+} else if (url == "/online/insurance/index.html") {
+
+        window.location.href="https://hcz-static.pingan.com.cn/online/insurance/index.html?mediaSource=SC03-YJXB-DIRECT-HCZ$SHOUYEGONGJULAN";
+
 } else {
     window.open("http://btp-oms-pac-openresty-prd.paic.com.cn/nets-tmr-pac-pad/viewCustomerVehicle.do", "target");
 }
